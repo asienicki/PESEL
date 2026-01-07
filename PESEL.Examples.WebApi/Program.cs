9546d1a7eaa7bc6a17eae581477019997cc9005f
Krzemik <artur.sienicki@gmail.com>
2026-01-07 13:27:24 +0100

    fix: trust forwarded headers behind reverse proxy and publish Docker image on release (#52)
    - enable forwarded headers handling for HTTPS behind Traefik
    - remove HTTPS redirection from container runtime
    - add Docker build & push step to release workflow using release version tags

--- PESEL.Examples.WebApi/Program.cs
+++ PESEL.Examples.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,12 @@ var app = builder.Build();
 
 app.EnableScalarDocs();
 
-app.UseHttpsRedirection();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto
+});
 
 app.MapPeselEndpoints();
 
