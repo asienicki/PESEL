913aa58bffd6c01457ddc6ca1718d13ef05534f2
Krzemik <artur.sienicki@gmail.com>
2026-01-10 16:32:20 +0100

    Refactor PeselGenerator and add overloads with full unit test coverage #88

---
+++ tests/PESEL.Tests/DomainTests/Algorithms/PeselGeneratorBuildPeselsFromRandomDigitsTakeTests.cs
@@ -0,0 +1,85 @@
+﻿namespace PESEL.Tests.DomainTests.Algorithms
+{
+
+
+    [TestClass]
+    [TestCategory("Domain")]
+    [TestCategory("Algorithm")]
+    [TestCategory("Generator")]
+    [TestCategory("Pesel")]
+    public class PeselGeneratorBuildPeselsFromRandomDigitsTakeTests
+    {
+        private const string DatePart = "740401";
+
+        [TestMethod]
+        public void BuildPeselsFromRandomDigits_TakeZero_ShouldGenerateAllPesels()
+        {
+            // Act
+            var result = PeselGenerator
+                .BuildPeselsFromRandomDigits(DatePart, take: 0)
+                .ToList();
+
+            // Assert
+            Assert.HasCount(10_000, result);
+        }
+
+        [TestMethod]
+        public void BuildPeselsFromRandomDigits_TakeOne_ShouldGenerateExactlyOnePesel()
+        {
+            // Act
+            var result = PeselGenerator
+                .BuildPeselsFromRandomDigits(DatePart, take: 1)
+                .ToList();
+
+            // Assert
+            Assert.HasCount(1, result);
+        }
+
+        [TestMethod]
+        public void BuildPeselsFromRandomDigits_TakeTen_ShouldGenerateExactlyTenPesels()
+        {
+            // Act
+            var result = PeselGenerator
+                .BuildPeselsFromRandomDigits(DatePart, take: 10)
+                .ToList();
+
+            // Assert
+            Assert.HasCount(10, result);
+        }
+
+        [TestMethod]
+        public void BuildPeselsFromRandomDigits_TakeEqualMax_ShouldGenerateExactly10000Pesels()
+        {
+            // Act
+            var result = PeselGenerator
+                .BuildPeselsFromRandomDigits(DatePart, take: 10_000)
+                .ToList();
+
+            // Assert
+            Assert.HasCount(10_000, result);
+        }
+
+        [TestMethod]
+        public void BuildPeselsFromRandomDigits_TakeGreaterThanMax_ShouldGenerateExactly10000Pesels()
+        {
+            // Act
+            var result = PeselGenerator
+                .BuildPeselsFromRandomDigits(DatePart, take: 20_000)
+                .ToList();
+
+            // Assert
+            Assert.HasCount(10_000, result);
+        }
+
+        [TestMethod]
+        public void BuildPeselsFromRandomDigits_Take_ShouldNotGenerateMoreThanRequested()
+        {
+            // Act
+            var result = PeselGenerator
+                .BuildPeselsFromRandomDigits(DatePart, take: 5);
+
+            // Assert
+            Assert.AreEqual(5, result.Take(6).Count());
+        }
+    }
+}
---
+++ tests/PESEL.Tests/DomainTests/Algorithms/PeselGeneratorDeterminePeselMonthTests.cs
@@ -0,0 +1,128 @@
+﻿namespace PESEL.Tests.DomainTests.Algorithms
+{
+    [TestClass]
+    [TestCategory("Domain")]
+    [TestCategory("Algorithm")]
+    [TestCategory("Generator")]
+    [TestCategory("Pesel")]
+    [TestCategory("Month")]
+    public class PeselGeneratorDeterminePeselMonthTests
+    {
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldReturnNull_ForFutureDate()
+        {
+            // Arrange
+            var date = DateTime.Now.AddDays(1);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldReturnNull_ForDateBefore1800()
+        {
+            // Arrange
+            var date = new DateTime(1700, 1, 1);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldEncodeMonth_For19thCentury()
+        {
+            // Arrange
+            var date = new DateTime(1850, 4, 15);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(84, result); // 4 + 80
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldEncodeMonth_For20thCentury()
+        {
+            // Arrange
+            var date = new DateTime(1974, 4, 15);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldEncodeMonth_For21stCentury()
+        {
+            // Arrange
+            var date = new DateTime(2005, 4, 15);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(24, result); // 4 + 20
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldHandleBoundaryYear_1899()
+        {
+            // Arrange
+            var date = new DateTime(1899, 12, 31);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(92, result); // 12 + 80
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldHandleBoundaryYear_1900()
+        {
+            // Arrange
+            var date = new DateTime(1900, 1, 1);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldHandleBoundaryYear_1999()
+        {
+            // Arrange
+            var date = new DateTime(1999, 12, 31);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(12, result);
+        }
+
+        [TestMethod]
+        public void DeterminePeselMonth_ShouldHandleBoundaryYear_2000()
+        {
+            // Arrange
+            var date = new DateTime(2000, 1, 1);
+
+            // Act
+            var result = PeselGenerator.DeterminePeselMonth(date);
+
+            // Assert
+            Assert.AreEqual(21, result); // 1 + 20
+        }
+    }
+}
